7b2c3eb709cb5132af8cfaea558fe5423aa8d659
Anthony Aricochea <aaricoch@everis.com>
2023-02-21 22:38:19 -0500

    list product-client

--- src/main/java/com/nttd/service/impl/InformationServiceImpl.java
+++ src/main/java/com/nttd/service/impl/InformationServiceImpl.java
@@ -1,7 +1,5 @@
 package com.nttd.service.impl;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,14 +9,12 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import com.nttd.api.AuditApi;
 import com.nttd.api.OperationApi;
 import com.nttd.api.response.AccountResponse;
-import com.nttd.api.response.OperationResponse;
-import com.nttd.dto.CustomerDto;
+import com.nttd.dto.ProductDto;
+import com.nttd.dto.ResponseDto;
 import com.nttd.service.InformationService;
 
-import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.core.Response;
 
 @ApplicationScoped
 public class InformationServiceImpl implements InformationService {
@@ -34,71 +30,64 @@ public class InformationServiceImpl implements InformationService {
 
     @ConfigProperty(name = "exception.general")
     String exceptionGeneral;
-    
 
     @Override
-    public Multi<List<AccountResponse>> getProducts(CustomerDto customerDto) {
- 
-            return operationApi.getProducts(customerDto.getCodigoCliente(),true)
-                .onItem().                
-                transformToMulti((prod) -> {
-                    /* 
-                    if(prod.getCode() == Response.Status.OK.getStatusCode()){
-                        obj.setCode(Response.Status.OK.getStatusCode());
-                        obj.setDescription(mensajeGeneral);
-                        List<AccountResponse> lista = new ArrayList<>();
-                        for(AccountResponse item : prod.getListaccount()){
-                            lista.add(new AccountResponse(
-                                item.getFlag_creation(),item.getCurrent_amount(),
-                                item.getStarting_amount()));                          
-                        }
-                        obj.setListaccount(lista);
-                    }else {
-                        obj.setCode(Response.Status.BAD_REQUEST.getStatusCode());
-                        obj.setDescription(exceptionGeneral);
+    public Uni<ResponseDto> getProducts(long id) {
+
+        return operationApi.getProducts(id, true)
+                .map(operation -> {
+                    List<AccountResponse> accounts = new ArrayList<>();
+                    if (operation.getCode() == 200) {
+                        accounts = operation.getListaccount();
+                    } else {
+                        accounts = null;
                     }
-                    return Uni.createFrom().item(obj);*/
-                    List<AccountResponse> lista = new ArrayList<>();
-                    for(AccountResponse item : prod.getListaccount()){
-                        lista.add(new AccountResponse(
-                            item.getFlag_creation(),item.getCurrent_amount(),
-                            item.getStarting_amount()));                          
+                    return accounts;
+                })
+                .map(accounts -> {
+                    if (accounts == null) {
+                        return new ResponseDto(400, exceptionGeneral, "Ocurrio un error en operationApi");
                     }
-                    return Multi.createFrom().item(lista);
-                });            
-      
-        
-        
-        
-                    
-                   
-                 
+                    List<ProductDto> products = new ArrayList<>();
+                    for (AccountResponse account : accounts) {
+                        ProductDto productDto = new ProductDto();
 
-                    /*AccountResponse obj = new AccountResponse();
-                    obj.setFlag_account(account.getFlag_account());
-                    return obj;*/
+                        productDto.setNombre(account.getDescription());
+                        productDto.setTipoProducto(account.getFlag_creation());
+                        productDto.setSaldo(account.getCurrent_amount());
 
-               /* });
-                .call((obj) -> {
+                        products.add(productDto);
+                    }
+                    return new ResponseDto(201, mensajeGeneral, products);
+                })
+                .onFailure().recoverWithItem(error -> new ResponseDto(400, exceptionGeneral, error.getMessage()));
 
-
-                    return Uni.createFrom().item(new ResponseDto(200, "OK", products));
-                });*/
-        
-        
-    /*
-            List<AccountResponse> accounts = operationRequest.getListaccount();
-            List<ProductDto> products = new ArrayList<>();
-            for (AccountResponse account : accounts) {
-                ProductDto productDto = new ProductDto();
-
-                productDto.setNombre(account.getDescription());
-                productDto.setTipoProducto(account.getFlag_creation());
-                productDto.setSaldo(account.getCurrent_amount());
-
-                products.add(productDto);
-            } */
-        
+        // .onItem().
+        // transformToMulti((prod) -> {
+        // /*
+        // if(prod.getCode() == Response.Status.OK.getStatusCode()){
+        // obj.setCode(Response.Status.OK.getStatusCode());
+        // obj.setDescription(mensajeGeneral);
+        // List<AccountResponse> lista = new ArrayList<>();
+        // for(AccountResponse item : prod.getListaccount()){
+        // lista.add(new AccountResponse(
+        // item.getFlag_creation(),item.getCurrent_amount(),
+        // item.getStarting_amount()));
+        // }
+        // obj.setListaccount(lista);
+        // }else {
+        // obj.setCode(Response.Status.BAD_REQUEST.getStatusCode());
+        // obj.setDescription(exceptionGeneral);
+        // }
+        // return Uni.createFrom().item(obj);*/
+        // List<AccountResponse> lista = new ArrayList<>();
+        // for(AccountResponse item : prod.getListaccount()){
+        // lista.add(new AccountResponse(
+        // item.getFlag_creation(),item.getCurrent_amount(),
+        // item.getStarting_amount()));
+        // }
+        // return Multi.createFrom().item(lista);
+        // });
 
     }
 
