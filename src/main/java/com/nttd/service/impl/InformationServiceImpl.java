f59aaa675df4521afb9622327fc9deaa24de3faa
Anthony Aricochea <aaricoch@everis.com>
2023-02-21 22:44:30 -0500

    list product-client add ID

--- src/main/java/com/nttd/service/impl/InformationServiceImpl.java
+++ src/main/java/com/nttd/service/impl/InformationServiceImpl.java
@@ -33,61 +33,40 @@ public class InformationServiceImpl implements InformationService {
 
     @Override
     public Uni<ResponseDto> getProducts(long id) {
+        try {
 
-        return operationApi.getProducts(id, true)
-                .map(operation -> {
-                    List<AccountResponse> accounts = new ArrayList<>();
-                    if (operation.getCode() == 200) {
-                        accounts = operation.getListaccount();
-                    } else {
-                        accounts = null;
-                    }
-                    return accounts;
-                })
-                .map(accounts -> {
-                    if (accounts == null) {
-                        return new ResponseDto(400, exceptionGeneral, "Ocurrio un error en operationApi");
-                    }
-                    List<ProductDto> products = new ArrayList<>();
-                    for (AccountResponse account : accounts) {
-                        ProductDto productDto = new ProductDto();
+            return operationApi.getProducts(id, true)
+                    .map(operation -> {
+                        List<AccountResponse> accounts = new ArrayList<>();
+                        if (operation.getCode() == 200) {
+                            accounts = operation.getListaccount();
+                        } else {
+                            accounts = null;
+                        }
+                        return accounts;
+                    })
+                    .map(accounts -> {
+                        if (accounts == null) {
+                            return new ResponseDto(400, exceptionGeneral, "Ocurrio un error en operationApi");
+                        }
+                        List<ProductDto> products = new ArrayList<>();
+                        for (AccountResponse account : accounts) {
+                            ProductDto productDto = new ProductDto();
 
-                        productDto.setNombre(account.getDescription());
-                        productDto.setTipoProducto(account.getFlag_creation());
-                        productDto.setSaldo(account.getCurrent_amount());
+                            productDto.setIdProducto(account.getIdAccountCustomer());
+                            productDto.setNombre(account.getDescription());
+                            productDto.setTipoProducto(account.getFlag_creation());
+                            productDto.setSaldo(account.getCurrent_amount());
 
-                        products.add(productDto);
-                    }
-                    return new ResponseDto(201, mensajeGeneral, products);
-                })
-                .onFailure().recoverWithItem(error -> new ResponseDto(400, exceptionGeneral, error.getMessage()));
+                            products.add(productDto);
+                        }
+                        return new ResponseDto(201, mensajeGeneral, products);
+                    })
+                    .onFailure().recoverWithItem(error -> new ResponseDto(400, exceptionGeneral, error.getMessage()));
 
-        // .onItem().
-        // transformToMulti((prod) -> {
-        // /*
-        // if(prod.getCode() == Response.Status.OK.getStatusCode()){
-        // obj.setCode(Response.Status.OK.getStatusCode());
-        // obj.setDescription(mensajeGeneral);
-        // List<AccountResponse> lista = new ArrayList<>();
-        // for(AccountResponse item : prod.getListaccount()){
-        // lista.add(new AccountResponse(
-        // item.getFlag_creation(),item.getCurrent_amount(),
-        // item.getStarting_amount()));
-        // }
-        // obj.setListaccount(lista);
-        // }else {
-        // obj.setCode(Response.Status.BAD_REQUEST.getStatusCode());
-        // obj.setDescription(exceptionGeneral);
-        // }
-        // return Uni.createFrom().item(obj);*/
-        // List<AccountResponse> lista = new ArrayList<>();
-        // for(AccountResponse item : prod.getListaccount()){
-        // lista.add(new AccountResponse(
-        // item.getFlag_creation(),item.getCurrent_amount(),
-        // item.getStarting_amount()));
-        // }
-        // return Multi.createFrom().item(lista);
-        // });
+        } catch (Exception ex) {
+            return Uni.createFrom().item(new ResponseDto(400, exceptionGeneral, ex.getMessage()));
+        }
 
     }
 
